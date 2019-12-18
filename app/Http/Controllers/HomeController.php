8e916c76c2a0c9bb30aa6d1550856f19650e79a3
mthdht <mthdht@gmail.com>
2019-12-18 11:36:38 +0100

    made route for dashboard and home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,15 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        if (\Auth::user()->role->role == 'user') {
+            return view('homeUser');
+        }
+        if (\Auth::user()->role->role == 'editor') {
+            return view('homeEditor');
+        }
+        if (\Auth::user()->role->role == 'admin') {
+            return view('homeAdmin');
+        }
+        return view('homeAdmin');
     }
 }
