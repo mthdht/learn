075a6741806bd22c59c8e768129b4a29c391f125
mthdht <mthdht@gmail.com>
2020-01-03 16:10:52 +0100

    v1 front index

--- resources/views/admin/cours/index.blade.php
+++ resources/views/admin/cours/index.blade.php
@@ -4,118 +4,29 @@
 <h3 class="text-2xl mb-8"><i class="fas fa-book-open fa-lg mr-4"></i>Tous les cours <a href="{{ route('admin.cours.create') }}" class="float-right"><i class="fas fa-plus-square fa-2x text-green-500"></i></a></h3>
 
 <div class="flex flex-wrap -mx-2">
+@foreach($categories as $category)
     <div class="w-full md:w-1/2 p-2">
         <div class="card shadow bg-white rounded flex">
-            <div class="w-16 bg-yellow-500 flex justify-center pt-8">
-                <i class="fab fa-js-square fa-2x text-white"></i>
+            <div class="w-16 bg-{{ $category->color }} flex justify-center pt-8">
+                <i class="{{ $category->icon }} fa-2x text-white"></i>
             </div>
             <div class="flex-grow">
-                <h3 class="border-b p-4 font-bold">Javascript</h3>
+                <h3 class="border-b p-4 font-bold">{{ $category->name }}</h3>
                 <table class="w-full">
-                    <tr class="hover:bg-gray-200">
-                        <td class="w-16"><i class="fab fa-js-square fa-2x px-4 py-2 text-yellow-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
+                @foreach($category->courses as $course)
+                    <tr class="hover:bg-gray-200 flex items-center">
+                        <td class="w-16"><i class="{{ $category->icon }} fa-2x px-4 py-2 text-{{ $category->color }}"></i></td>
+                        <td class="p-2 flex-grow">{{ $course->title }}</td>
+                        <td class="w-24 p-2 flex justify-between">
+                            <a href="{{ route('admin.cours.show', ['course' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
+                            <a href="{{ route('admin.cours.edit', ['course' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
                         </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-js-square fa-2x px-4 py-2 text-yellow-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-js-square fa-2x px-4 py-2 text-yellow-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-js-square fa-2x px-4 py-2 text-yellow-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-js-square fa-2x px-4 py-2 text-yellow-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
+                    </tr>  
+                @endforeach
                 </table>
             </div>
         </div>
     </div>
-
-    <div class="w-full md:w-1/2 p-2">
-        <div class="card shadow bg-white rounded flex">
-            <div class="w-16 bg-purple-500 flex justify-center pt-8">
-                <i class="fab fa-php fa-2x text-white"></i>
-            </div>
-            <div class="flex-grow">
-                <h3 class="border-b p-4 font-bold">PHP</h3>
-                <table class="w-full">
-                    <tr class="hover:bg-gray-200">
-                        <td class="w-16"><i class="fab fa-php fa-2x px-4 py-2 text-purple-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-php fa-2x px-4 py-2 text-purple-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-php fa-2x px-4 py-2 text-purple-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-php fa-2x px-4 py-2 text-purple-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-
-                    <tr class="hover:bg-gray-200">
-                        <td><i class="fab fa-php fa-2x px-4 py-2 text-purple-500"></i></td>
-                        <td class="p-2">Les conditions</td>
-                        <td class="p-2 flex justify-between">
-                            <a href="{{ route('admin.cours.show', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-blue-500 rounded text-white"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('admin.cours.edit', ['cour' => 'conditions']) }}" class="w-8 h-8 flex items-center justify-center bg-orange-500 rounded text-white"><i class="fas fa-pencil-alt"></i></a>
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
+@endforeach
 </div>
 @endsection
