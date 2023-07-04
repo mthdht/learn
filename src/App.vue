43009e8583366bb0bb03d93a1e2567d4fde2df31
mthdht <mthdht@gmail.com>
2023-07-04 08:32:45 +0200

    install router, tailwind, heroicon, headlessui

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <Header></Header>
+  <p>
+    <!-- use the router-link component for navigation. -->
+    <!-- specify the link by passing the `to` prop. -->
+    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/dashboard">Go to dashboard</router-link>
+  </p>
+  <!-- route outlet -->
+  <!-- component matched by the route will render here -->
+  <router-view></router-view>
 </template>
 
-<script setup>
-import Header from './components/Header.vue';
-</script>
+<script setup></script>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import Dashboard from './components/Dashboard.vue';
+import Home from './components/Home.vue';
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: Home },
+  { path: '/dashboard', component: Dashboard },
+];
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+});
+
+createApp(App).use(router).mount('#app');
