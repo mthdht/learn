ade74e9ca6f2cb8821f10da6519ed81f89ce047a
mthdht <mthdht@gmail.com>
2023-07-04 13:34:22 +0200

    made navbar layout

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <template>
-  <p>
-    <!-- use the router-link component for navigation. -->
-    <!-- specify the link by passing the `to` prop. -->
-    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-    <router-link to="/">Go to Home</router-link>
-    <router-link to="/dashboard">Go to dashboard</router-link>
-  </p>
-  <!-- route outlet -->
-  <!-- component matched by the route will render here -->
+  <Navbar></Navbar>
+
   <router-view></router-view>
 </template>
 
-<script setup></script>
+<script setup>
+import Navbar from './components/Navbar.vue';
+</script>
 
 <style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="navbar">
+    <div
+      class="navbar-container flex gap-4 container mx-auto font-semibold py-2"
+    >
+      <div class="navbar-brand bg-red-500 w-32"></div>
+      <div class="navbar-links flex justify-center gap-4 grow">
+        <router-link to="/">Go to Home</router-link>
+        <router-link to="/dashboard">Go to dashboard</router-link>
+      </div>
+      <div class="navbar-actions w-24 bg-green-500"></div>
+    </div>
+  </header>
+</template>
+
+<script setup></script>
