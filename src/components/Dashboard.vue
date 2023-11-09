09f02ab0e588f95e3710db20c6d79e3c1744a89c
mthdht <mthdht@gmail.com>
2023-11-09 09:47:25 +0100

    made section with statistics card components

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,98 +2,30 @@
   <main class="grow flex overflow-hidden">
     <Sidebar> </Sidebar>
     <div class="content grow overflow-auto p-4 pt-8">
-      <section>
-        <div class="stat-card w-1/4 rounded overflow-hidden font-semibold">
-          <div
-            class="
-              stat-card-label
-              flex
-              justify-between
-              bg-gradient-to-r
-              from-sky-500 from-30%
-              to-sky-400
-              text-slate-50
-              p-4
-            "
-          >
-            <p>Lorem, ipsum dolor.</p>
+      <section class="grid md:grid-cols-2 lg:grid-cols-4 gap-2">
+        <StatisticsCard color="red">
+          <template #label>Cours</template>
+          <template #badge>99+</template>
+          <template #action></template>
+        </StatisticsCard>
 
-            <span
-              class="
-                h-7
-                w-7
-                rounded-full
-                bg-sky-500
-                flex
-                justify-center
-                items-center
-              "
-              >7</span
-            >
-          </div>
-          <router-link
-            :to="{ name: 'user.lessons', params: { user: 'mthdht' } }"
-            class="
-              stat-card-action
-              flex
-              justify-between
-              items-center
-              py-2
-              px-4
-              bg-sky-600
-              text-slate-100
-              w-full
-            "
-            ><p>Voir plus</p>
-            <ArrowSmallRightIcon class="h-5 w-5 stroke-3"></ArrowSmallRightIcon
-          ></router-link>
-        </div>
+        <StatisticsCard color="yellow">
+          <template #label>Cours</template>
+          <template #badge>99+</template>
+          <template #action></template>
+        </StatisticsCard>
 
-        <div class="stat-card w-1/4 rounded overflow-hidden font-semibold">
-          <div
-            class="
-              stat-card-label
-              flex
-              justify-between
-              bg-gradient-to-r
-              from-sky-500 from-30%
-              to-sky-400
-              text-slate-50
-              p-4
-            "
-          >
-            <p>Lorem, ipsum dolor.</p>
+        <StatisticsCard color="green">
+          <template #label>Cours</template>
+          <template #badge>99+</template>
+          <template #action></template>
+        </StatisticsCard>
 
-            <span
-              class="
-                h-7
-                w-7
-                rounded-full
-                bg-sky-500
-                flex
-                justify-center
-                items-center
-              "
-              >7</span
-            >
-          </div>
-          <router-link
-            :to="{ name: 'user.lessons', params: { user: 'mthdht' } }"
-            class="
-              stat-card-action
-              flex
-              justify-between
-              items-center
-              py-2
-              px-4
-              bg-sky-600
-              text-slate-100
-              w-full
-            "
-            ><p>Voir plus</p>
-            <ArrowSmallRightIcon class="h-5 w-5 stroke-3"></ArrowSmallRightIcon
-          ></router-link>
-        </div>
+        <StatisticsCard color="blue">
+          <template #label>Cours</template>
+          <template #badge>99+</template>
+          <template #action></template>
+        </StatisticsCard>
       </section>
     </div>
   </main>
@@ -101,5 +33,6 @@
 
 <script setup>
 import Sidebar from './Sidebar.vue';
+import StatisticsCard from './StatisticsCard.vue';
 import { ArrowSmallRightIcon } from '@heroicons/vue/24/outline';
 </script>
--- src/components/StatisticsCard.vue
+++ src/components/StatisticsCard.vue
@@ -1,26 +1,27 @@
 <template>
-  <div class="stat-card w-1/4 rounded overflow-hidden font-semibold">
+  <div class="stat-card rounded overflow-hidden font-semibold">
     <div
       class="
         stat-card-label
         flex
         justify-between
         bg-gradient-to-r
-        from-sky-500 from-30%
-        to-sky-400
         text-slate-50
         p-4
       "
+      :class="labelColor"
     >
-      <p>Lorem, ipsum dolor.</p>
+      <slot name="label">Lessons</slot>
 
       <span
-        class="h-7 w-7 rounded-full bg-sky-500 flex justify-center items-center"
-        >7</span
+        class="h-9 w-9 rounded-full flex justify-center items-center"
+        :class="badgeColor"
       >
+        <slot name="badge">1</slot>
+      </span>
     </div>
     <router-link
-      :to="{ name: 'user.lessons', params: { user: 'mthdht' } }"
+      :to="props.to"
       class="
         stat-card-action
         flex
@@ -28,34 +29,75 @@
         items-center
         py-2
         px-4
-        bg-sky-600
         text-slate-100
         w-full
       "
-      ><p>Voir plus</p>
+      :class="actionColor"
+      ><p>
+        <slot name="action">See more</slot>
+      </p>
       <ArrowSmallRightIcon class="h-5 w-5 stroke-3"></ArrowSmallRightIcon
     ></router-link>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import { ArrowSmallRightIcon } from '@heroicons/vue/24/outline';
+
 const props = defineProps({
   color: {
     type: String,
     default: 'gray',
     validator(value) {
       // The value must match one of these strings
-      return ['gray', 'red', 'orange', 'yellow', 'blue'].includes(value);
+      return ['gray', 'red', 'orange', 'yellow', 'green', 'blue'].includes(
+        value
+      );
     },
+  },
+  to: {
+    type: Object,
+    default: { name: 'user.dashboard', params: { user: 'mthdht' } },
   },
 });
 
-const backgroundColor = {
-  gray: 'from-slate-500 from-30% to-slate-400',
-  red: 'from-red-500 from-30% to-red-400',
-  orange:'from-orange-500 from-30% to-orange-400',
-  yellow: 'from-yellow-500 from-30% to-yellow-400',
-  green: 'from-emerald-500 from-30% to-emerald-400'
-  blue: 'from-sky-500 from-30% to-sky-400'
-}
+const labelColor = computed(() => {
+  return colors['label'][props.color] ?? '';
+});
+
+const actionColor = computed(() => {
+  return colors['action'][props.color] ?? '';
+});
+
+const badgeColor = computed(() => {
+  return colors['badge'][props.color] ?? '';
+});
+
+const colors = {
+  label: {
+    gray: 'from-slate-500 from-30% to-slate-400',
+    red: 'from-red-500 from-30% to-red-400',
+    orange: 'from-orange-500 from-30% to-orange-400',
+    yellow: 'from-yellow-500 from-30% to-yellow-400',
+    green: 'from-emerald-500 from-30% to-emerald-400',
+    blue: 'from-sky-500 from-30% to-sky-400',
+  },
+  action: {
+    gray: 'bg-slate-500',
+    red: 'bg-red-500',
+    orange: 'bg-orange-500',
+    yellow: 'bg-yellow-500',
+    green: 'bg-emerald-500',
+    blue: 'bg-sky-500',
+  },
+  badge: {
+    gray: 'bg-slate-600',
+    red: 'bg-red-600',
+    orange: 'bg-orange-600',
+    yellow: 'bg-yellow-600',
+    green: 'bg-emerald-600',
+    blue: 'bg-sky-600',
+  },
+};
 </script>
