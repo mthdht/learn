ab8762afa9c7a1fb693d177cb2210832594629b1
mthdht <mthdht@gmail.com>
2024-01-22 09:58:43 +0100

    start lesson card for gallery

---
+++ src/components/LessonCard.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script setup>
+const props = defineProps({
+  layout: {
+    type: String,
+    default: 'vertical',
+    validator(value) {
+      return ['vertical', 'horizontal'].includes(value);
+    },
+  },
+});
+</script>
