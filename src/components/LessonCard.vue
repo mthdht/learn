cd437e8032e168574a6f280148349c2b7ba23e68
mthdht <mthdht@gmail.com>
2024-01-23 08:54:08 +0100

    add hover scale effect and made on lesson card and made it linkable

--- src/components/LessonCard.vue
+++ src/components/LessonCard.vue
@@ -1,23 +1,36 @@
 <template>
-  <div class="l-lesson-card rounded overflow-hidden shadow">
-    <div class="l-lesson-card-image aspect-video bg-sky-600">
-      <img />
-    </div>
-    <div class="l-lesson-card-content bg-slate-50 p-4">
-      <h4 class="uppercase">Some Categories</h4>
-      <p>The title of the lesson</p>
-      <div class="infos flex gap-4">
-        <div class="flex gap-2 items-center">
-          <ClockIcon class="h-5 w-5"></ClockIcon>
-          <span>6 hours</span>
-        </div>
-        <div class="flex gap-2 items-center">
-          <ChartBarIcon class="h-5 w-5"></ChartBarIcon>
-          <span>facile</span>
+  <router-link to="">
+    <div
+      class="
+        l-lesson-card
+        rounded
+        overflow-hidden
+        shadow
+        hover:scale-105
+        transition-transform
+      "
+    >
+      <div class="l-lesson-card-image aspect-video bg-sky-600">
+        <img />
+      </div>
+      <div class="l-lesson-card-content bg-slate-50 p-4">
+        <h4 class="uppercase text-sky-800 font-light tracking-tight">
+          Some Categories
+        </h4>
+        <p class="text-slate-700 font-semibold mt-1">The title of the lesson</p>
+        <div class="infos flex gap-4 justify-end mt-4">
+          <div class="flex gap-2 items-center">
+            <ClockIcon class="h-5 w-5"></ClockIcon>
+            <span>6 hours</span>
+          </div>
+          <div class="flex gap-2 items-center">
+            <ChartBarIcon class="h-5 w-5"></ChartBarIcon>
+            <span>facile</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script setup>
