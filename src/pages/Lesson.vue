aac0f8f72f80a0b36107b736e0ee21e11043d9ae
mthdht <mthdht@gmail.com>
2024-01-25 09:14:31 +0100

    start header on lesson template

--- src/pages/Lesson.vue
+++ src/pages/Lesson.vue
@@ -1,9 +1,29 @@
 <template>
-  <main class="grow container p-4">
-    <header>
-      <h2>titre de la leçon</h2>
+  <main class="p-4 bg-sky-50">
+    <header class="py-8 container">
+      <p class="uppercase text-sky-800 font-semibold tracking-tight">
+        JAVAscript
+      </p>
+      <h2 class="text-4xl font-semibold mb-8">titre de la leçon</h2>
 
-      <div class="prerequisites-and-goal">
+      <p class="text-slate-600 mb-4">
+        Python est très demandé et accessible pour les débutants. Apprenez à
+        coder avec Python pour écrire des programmes simples mais puissants, et
+        pour automatiser les tâches.
+      </p>
+
+      <div class="infos flex gap-4 mt-4">
+        <div class="flex gap-2 items-center">
+          <ClockIcon class="h-5 w-5"></ClockIcon>
+          <span>1 hour</span>
+        </div>
+        <div class="flex gap-2 items-center">
+          <ChartBarIcon class="h-5 w-5"></ChartBarIcon>
+          <span>easy</span>
+        </div>
+      </div>
+
+      <div class="prerequisites-and-goal mt-4">
         <ul class="list-disc list-inside">
           <li>Prérequis: Javascript, php, variable</li>
           <li>
@@ -13,6 +33,13 @@
         </ul>
       </div>
     </header>
-    <section></section>
+    <section class="content flex gap-4">
+      <section class="w-3/4"></section>
+      <aside class="grow"></aside>
+    </section>
   </main>
 </template>
+
+<script setup>
+import { ClockIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
+</script>
